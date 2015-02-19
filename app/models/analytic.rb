a4e49aff1eb45c1de04c8f01a3a22ddbdb9c4778
Chris C Cerami <chrisccerami@gmail.com>
2015-02-19 01:41:58 -0500

    Analytics track browser data for unique visitors

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -4,4 +4,18 @@ class Analytic < ActiveRecord::Base
 
   validates :visits, presence: true, numericality: true
   validates :unique_visits, presence: true, numericality: true
+
+  def most_common_browser
+    browsers = unique_visitors.all.group_by{ |uv| uv.browser }
+    if browsers.any?
+      browser = browsers.sort_by { |b| b.length }.first.first
+    end
+  end
+
+  def most_common_platform
+    browsers = unique_visitors.all.group_by{ |uv| uv.platform }
+    if browsers.any?
+      browser = browsers.sort_by { |b| b.length }.first.first
+    end
+  end
 end
